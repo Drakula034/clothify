d146c8f321de87efc8ab2c34bbd21f1035a98884
Drakula034 <abhishek034nits@gmail.com>
2024-05-27 15:18:37 +0530

    broken our homepage into smaller component and finsihed styling our menu item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div>Hello World</div>;
+function App () {
+  return <div><HomePage/></div>;
 }
 
 export default App;
