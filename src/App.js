2f6951f2e1a3557761a337c6f127b40faa900628
Drakula034 <abhishek034nits@gmail.com>
2024-05-30 10:30:41 +0530

    navigate to dashboard after signin

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shoppage/shoppage.component.jsx";
 import SignInAndOutPage from "./pages/sign-in-out-page/sign-in-out.component.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/header/header.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 import { setCurrentUser } from "./redux/userSlice.jsx";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user.currentUser);
 
   useEffect(() => {
     const subscribe = auth.onAuthStateChanged(async (userAuth) => {
@@ -43,7 +44,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndOutPage />} />
+          <Route
+            exact
+            path="/signin"
+            element={currentUser ? <Navigate to="/" /> : <SignInAndOutPage />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
